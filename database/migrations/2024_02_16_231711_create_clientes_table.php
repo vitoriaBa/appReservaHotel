833a4ff79418300cab00be83fd13a9161e3c12ed
vitoriaBa <vitoriabatista877@gmail.com>
2024-02-16 20:30:11 -0300

    migration

--- database/migrations/2024_02_16_231711_create_clientes_table.php
+++ database/migrations/2024_02_16_231711_create_clientes_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clientes', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
-            $table->string('email')->unique();
-          /*  $table->timestamp('email_verified_at')->nullable();*/
-            $table->string('fone');
-         /*   $table->rememberToken();*/
             $table->timestamps();
         });
     }
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clientes');
     }
 };
